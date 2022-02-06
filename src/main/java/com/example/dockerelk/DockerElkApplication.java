d87eccd078a0b2f35c4b09382df8b02af14f109d
jaeho <jhdl0157@naver.com>
2022-02-06 21:02:50 +0900

    first commit

---
+++ src/main/java/com/example/dockerelk/DockerElkApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerElkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockerelk/DockerElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerelk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerElkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
