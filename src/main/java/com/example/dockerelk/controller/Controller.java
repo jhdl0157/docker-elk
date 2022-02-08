2cd787ef1e71bd2dfc3a7a0cc9336b877943437e
jaeho <jhdl0157@naver.com>
2022-02-08 14:15:54 +0900

    add setting

---
+++ src/main/java/com/example/dockerelk/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.example.dockerelk.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class Controller {
+    private final Logger logger = LoggerFactory.getLogger("LoggerController 의 로그");
+    @GetMapping("/log")
+    public void log() {
+        logger.error("로깅 발생!");
+    }
+}
